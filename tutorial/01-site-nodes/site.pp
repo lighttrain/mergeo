6572260a9321201cead44fe1b8a40dfcea11d5b1
Graham M <bouncy_castle_buttocks@hotmail.com>
2019-11-07 16:05:05 +0000

    edited site.pp for timknob  feature-7 GM

--- tutorial/01-site-nodes/site.pp
+++ tutorial/01-site-nodes/site.pp
@@ -1,6 +1,6 @@
 #
-# site.pp
-# manifest for general configuration-managed hosts
+# site.pp you knob
+# manifes  for general configuration-managed hosts
 #
 # Command to execute:
 #     puppet apply site.pp
